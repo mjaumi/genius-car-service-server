10b8a6d09be07e14f2fa74442e753b9dd86a51d8
mjaumi <mjaumi2864@gmail.com>
2022-04-25 23:19:13 +0600

    initial commit

---
+++ __server_deploy.js
@@ -0,0 +1,16 @@
+/**
+ * ------------------------
+ * ONE TIME FOR PC
+ * ------------------------
+ * 1. Create Heroku account
+ * 2. Verify Email
+ * 3. Install Heroku CLI [npm install -g heroku]
+ * 4. heroku login
+ * 
+ * -------------------------
+ * FOR EACH PROJECT
+ * -------------------------
+ * 1. heroku create
+ * 2. make sure you: git add ., git commit, git push
+ * 3. git push heroku main
+ */
